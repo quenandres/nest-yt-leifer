b1d887efc91ec0295f27c0dcad000792302db2b3
Jhonatan Mejia <quenandres@gmail.com>
2022-12-29 20:14:06 -0500

    Add: Comando para crear el resource (crud)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,6 +5,11 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get('users')
+  getUsers(): string[] {
+    return this.appService.getUsers();
+  }
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getUsers(): string[] {
+    return ['quenan', 'dres'];
+  }
 }
